244bd0c79d13d0a82e72a976946a360f9cfdb4d3
Josef Patoprsty <seppi@josefnpat.com>
2013-11-10 00:52:24 -0500

    Stuff

--- dmg01.lua
+++ dmg01.lua
@@ -7,8 +7,88 @@ dmg01.scale_max = 8
 dmg01.w = 160
 dmg01.h = 144
 
+dmg01.palette = {}
+
+-- Default color palette. Source:
+-- http://en.wikipedia.org/wiki/List_of_video_game_console_palettes#Original_Game_Boy
+dmg01.palette[1] = {
+  name = "default",
+  colors = {
+    { 15/255, 56/255, 15/255},
+    { 48/255, 98/255, 48/255},
+    {139/255,172/255, 15/255},
+    {155/255,188/255, 15/255}
+  }
+}
+
+-- Hardcore color profiles. Source:
+-- http://www.hardcoregaming101.net/gbdebate/gbcolours.htm
+
+dmg01.palette[2] = {
+  name = "dark_yellow",
+  colors = {
+    {33/255,32/255,16/255},
+    {107/255,105/255,49/255},
+    {181/255,174/255,74/255},
+    {255/255,247/255,123/255}
+  }
+}
+
+dmg01.palette[3] = {
+  name = "light_yellow",
+  colors = {
+    {102/255,102/255,37/255},
+    {148/255,148/255,64/255},
+    {208/255,208/255,102/255},
+    {255/255,255/255,148/255}
+  }
+}
+
+dmg01.palette[4] = {
+  name = "green",
+  colors = {
+    {8/255,56/255,8/255},
+    {48/255,96/255,48/255},
+    {136/255,168/255,8/255},
+    {183/255,220/255,17/255}
+  }
+}
+
+dmg01.palette[5] = {
+  name = "greyscale",
+  colors = {
+    {56/255,56/255,56/255},
+    {117/255,117/255,117/255},
+    {178/255,178/255,178/255},
+    {239/255,239/255,239/255}
+  }
+}
+
+dmg01.palette[6] = {
+  name = "stark_bw",
+  colors = {
+    {0/255,0/255,0/255},
+    {117/255,117/255,117/255},
+    {178/255,178/255,178/255},
+    {255/255,255/255,255/255}
+  }
+}
+
+dmg01.palette[7] = {
+  name = "pocket",
+  colors = {
+    {108/255,108/255,78/255},
+    {142/255,139/255,87/255},
+    {195/255,196/255,165/255},
+    {227/255,230/255,201/255}
+  }
+}
+
+dmg01.current_palette = 1
+
 function dmg01.pre_draw()
   love.graphics.setPixelEffect(dmg01.shader)
+  dmg01.shader:send('COLOR_MASKS',unpack(dmg01.palette[dmg01.current_palette].colors))
   love.graphics.push()
   love.graphics.scale(dmg01.scale)
 end
@@ -26,17 +106,12 @@ dmg01.shader = love.graphics.newPixelEffect [[
 
 extern number value;
 
-// http://www.hardcoregaming101.net/gbdebate/gbcolours.htm
-
-// http://en.wikipedia.org/wiki/List_of_video_game_console_palettes#Original_Game_Boy
-uniform vec3 COLOR_MASKS[ 4 ] = { vec3( 0.058823529, 0.21960784, 0.019607843),
-                                  vec3( 0.18823529 , 0.38431373, 0.18823529 ),
-                                  vec3( 0.54509804 , 0.6745098 , 0.058823529),
-                                  vec3( 0.60784314 , 0.7372549 , 0.058823529) };
+uniform vec3 COLOR_MASKS[ 4 ];
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords){
 			vec4 pixel = Texel(texture, texture_coords);
-			float avg = max(0, ((pixel.r + pixel.g + pixel.b)/3)+value/10);
+//			float avg = max(0, ((pixel.r + pixel.g + pixel.b)/3)+value/10);
+			float avg = (pixel.r + pixel.g + pixel.b)/3;
 			int index = int(avg*4);
 
 			pixel.r = COLOR_MASKS[index][0];
--- main.lua
+++ main.lua
@@ -1,17 +1,45 @@
-cat = love.graphics.newImage("cat.png")
-cat:setFilter("nearest","nearest")
+files_uri = love.filesystem.enumerate("images")
+
+images = {}
+for i,v in pairs(files_uri) do
+  if string.sub(v,-4,-1) == ".png" then
+    images[i] = love.graphics.newImage("images/"..v)
+    images[i]:setFilter("nearest","nearest")  
+  end
+end
+
+current_img = math.random(1,#images)
+
+font_raw = love.graphics.newImage("imagefont.png")
+font_raw:setFilter("nearest","nearest")
+
+font = love.graphics.newImageFont(font_raw,
+  " ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-:;.,/\\+-*!?'\")(][}{#$<>&@%")
+
+love.graphics.setFont(font)
 
 dmg = require('dmg01')
+
+speed = 10
 
 function love.draw()
   dmg.pre_draw()
   for x = 0,1 do
     for y = 0,1 do
-      love.graphics.draw(cat,
-        math.floor(offset*100)%(dmg.w)-dmg.w*x,
-        math.floor(offset*100)%(dmg.h)-dmg.h*y)
+      love.graphics.draw(images[current_img],
+        math.floor(offset*speed)%(dmg.w)-dmg.w*x,
+        math.floor(offset*speed)%(dmg.h)-dmg.h*y)
     end
   end
+  love.graphics.setColor(255,255,255)
+  love.graphics.rectangle("fill",1,1,dmg.w-2,8*3+1)
+  love.graphics.setColor(0,0,0)
+  love.graphics.print(
+    "PALETTE(L/R):"..string.upper(dmg.palette[dmg.current_palette].name)..
+    "\nSCALE(U/D):"..dmg.scale..
+    "\nIMG(SPACE):"..current_img,
+    2,2)
+  love.graphics.setColor(255,255,255)
   dmg.post_draw()
 end
 
@@ -28,6 +56,21 @@ function love.keypressed(key)
   elseif key == "down" then
     if dmg.scale/2 >= dmg.scale_min then
       dmg:setScale(dmg.scale/2)
+    end
+  elseif key == "left" then
+    dmg.current_palette = dmg.current_palette - 1
+    if dmg.current_palette < 1 then
+      dmg.current_palette = #dmg.palette
+    end
+  elseif key == "right" then
+    dmg.current_palette = dmg.current_palette + 1
+    if dmg.current_palette > #dmg.palette then
+      dmg.current_palette = 1
+    end
+  elseif key == " " then
+    current_img = current_img + 1
+    if current_img > #images then
+      current_img = 1
     end
   end
 end
